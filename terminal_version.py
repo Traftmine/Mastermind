e4a3fd7812c339674c809facfc2f41d65e1bb635
Traftmine <leo.royer.1@etu.u-bordeaux.fr>
2024-02-09 11:45:38 +0100

    solution option

--- terminal_version.py
+++ terminal_version.py
@@ -8,14 +8,19 @@ def game_start():
         game.append(rm.randint(1,5))
     return game
 
+def solution(game):
+    print("Here is the solution : " + str(game))
+
 def main():
     game = game_start()
-    print(game)
     Not_correct = True
     guesses = [False for i in range(4)]
     guesses_for_user = ["" for i in range(4)]
     while(Not_correct):
         guess = list(map(int,(input("Your guess : ")).split(' ')))
+        if guess == [0, 0, 0, 0]:
+            solution(game)
+            return 0
         i = 0
         for e in guess:
             if e == game[i]:
